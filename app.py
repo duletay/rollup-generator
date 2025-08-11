cd5a13deb7e2e4ca8b5d0984af10b92ccd8cea03
duletay <duletay@gmail.com>
2025-08-11 15:59:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -385,4 +385,10 @@ def generate_zip():
     )
 
 if __name__ == "__main__":
+
     app.run(debug=False, port=5000)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
